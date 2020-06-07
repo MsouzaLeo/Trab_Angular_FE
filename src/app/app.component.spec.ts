338bb44b8599ab6dd86197e7b66ac922a1ba06b1
Leonardo Martins Souza <leonardo.msouza@hotmail.com>
2020-06-07 17:51:32 -0300

    changed the app folder

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,20 +12,20 @@ describe('AppComponent', () => {
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Trab3'`, () => {
+  it(`should have as title 'exerc-aula14'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('Trab3');
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('exerc-aula14');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Trab3 app is running!');
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('exerc-aula14 app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Order } from './order'; 
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Trab3';
+  title = 'Awesome Shop';
+  authors = 'Upsorn';
+
+  drinks = ['Coffee', 'Tea', 'Milk'];
+
+  orderModel = new Order('someone', 'someone@uva.edu', 999, '', '', true);
+
+  confirm_msg = 'before confirm';
+  data_submitted = '';
+
+  confirmOrder(data) {
+     console.log(data);
+     this.confirm_msg = 'Thank you, ' + data.name + '(' + data.name.length + ')';
+     this.confirm_msg += '. You ordered ' + data.drink_option;
+  }
+
 }
